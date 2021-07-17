214baba368adb360d220d11b766145004487d0e5
beuschl <beuschl@hotmail.com>
2021-07-17 08:17:50 +0200

    reward::manager interface changes

--- src/reward/inc/reward/manager.hpp
+++ src/reward/inc/reward/manager.hpp
@@ -8,14 +8,16 @@
 namespace nexuspool {
 namespace reward {
 
+enum class Difficulty_result : std::uint8_t { accept = 0, reject, block_found };
+
 class Manager {
 public:
     using Uptr = std::unique_ptr<Manager>;
 
     virtual ~Manager() = default;
 
-    // get the total weight for the current round.
-    virtual std::uint64_t get_total_weight() const = 0;
+    // get the total shares for the current round.
+    virtual std::uint64_t get_total_shares() const = 0;
 
     virtual std::uint64_t get_reward_of_miner(uint256_t session_key) const = 0;
     virtual std::uint64_t get_shares_of_miner(uint256_t session_key) const = 0;
@@ -23,7 +25,7 @@ public:
     // Clear the Shares for the current round.
     virtual void clear_shares() = 0;
 
-    virtual bool check_difficulty(double difficulty, double target_difficulty) const = 0;
+    virtual Difficulty_result check_difficulty(double difficulty, double target_difficulty) const = 0;
 
 
 };
