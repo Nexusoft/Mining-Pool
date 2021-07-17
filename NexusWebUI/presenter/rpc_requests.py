bb54c1709b8911cfb2d6773c9259617d860d8511
nurglexxx <ben.vees@gmail.com>
2021-07-17 16:23:35 +0200

    New Connection Method via plain socket

--- NexusWebUI/presenter/rpc_requests.py
+++ NexusWebUI/presenter/rpc_requests.py
@@ -1,12 +1,21 @@
 import json
-
+import socket
 import requests
+import pjrpc
+from pjrpc.client.backend import requests as pjrpc_client
 
 
 def get_latest_blocks(_url):
     payload = {"jsonrpc": "2.0", "method": "get_latest_blocks", "id": 1}
 
-    response = requests.post(url=_url, json=payload)
+    # response = requests.request(method='get_latest_blocks', url=_url)
+
+    s = socket.socket()
+    s.connect(('127.0.0.1', 5000))
+    s.send(json.dumps(payload).encode('utf-8'))
+
+    response = s.recv(1024)
+
     json_data = json.loads(response.text)
     sorted_obj = dict(json_data)
     sorted_obj['result'] = sorted(json_data['result'], key=lambda x: x['height'], reverse=True)
