86a82b8cfbffe8b580519e8df20d2a84955627fa
beuschl <beuschl@hotmail.com>
2021-07-20 15:59:09 +0200

    added persistance config options

--- src/config/inc/config/types.hpp
+++ src/config/inc/config/types.hpp
@@ -8,17 +8,36 @@ namespace nexuspool
 {
 namespace config
 {
-	enum class Mining_mode : std::uint8_t
-	{
-		PRIME = 0,
-		HASH = 1
-	};
+enum class Mining_mode : std::uint8_t
+{
+	PRIME = 0,
+	HASH = 1
+};
 
-    enum class Stats_printer_mode : std::uint8_t
-	{
-		CONSOLE = 0,
-		FILE
-	};
+enum class Stats_printer_mode : std::uint8_t
+{
+	CONSOLE = 0,
+	FILE
+};
+
+enum class Persistance_type : std::uint8_t 
+{
+	database = 0,
+	sqlite
+};
+
+struct Persistance_config
+{
+	Persistance_type m_type{ Persistance_type::database};
+	std::string m_file{};
+};
+
+struct Pool_config
+{
+	std::string m_address{};
+	std::uint16_t m_fee{};
+	std::uint32_t m_min_share{};
+};
 }
 }
 #endif
