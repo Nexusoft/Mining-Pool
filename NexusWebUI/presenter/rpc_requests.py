062074e2c4ebc282ae4fcf6b60569562ca7bfe14
nurglexxx <ben.vees@gmail.com>
2021-07-20 19:54:17 +0200

    get_meta_info implemented (prototype)

--- NexusWebUI/presenter/rpc_requests.py
+++ NexusWebUI/presenter/rpc_requests.py
@@ -5,27 +5,42 @@ import pybson
 import bson
 
 
-def get_latest_blocks(_url):
+def socket_connect(_ip, _port):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((_ip, _port))
+    return s
+
+
+
+def socket_disconnect(_socket):
+    _socket.close()
+
+
+def get_latest_blocks(_socket):
+    # Todo Documentation
+    # Todo Error Handling
+
     payload = {"jsonrpc": "2.0", "method": "get_latest_blocks", "id": 1}
 
-    s = socket.socket()
-    s.connect(('127.0.0.1', 5000))
-    s.send(pybson.dumps(payload))
+    # s = socket.socket()
+    # s.connect(('127.0.0.1', 5000))
+    # s.send(pybson.dumps(payload))
+    _socket.send(pybson.dumps(payload))
 
-    response = s.recv(20000)
-
+    # response = s.recv(20000)
+    response = _socket.recv(20000)
     json_data = bson.loads(response)
-
-    print(f"Result: {json_data['result']}")
 
     return json_data
 
 
-def get_meta_info(_url):
+def get_meta_info(_socket):
     payload = {"jsonrpc": "2.0", "method": "get_meta_info", "id": 1}
 
-    response = requests.post(url=_url, json=payload)
-    json_data = json.loads(response.text)
+    _socket.send(pybson.dumps(payload))
+    response = _socket.recv(1024)
+    json_data = bson.loads(response)
 
     return json_data
 
