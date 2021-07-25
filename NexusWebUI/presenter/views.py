86df1e57ab33a24a8a3afacb4b442c4bc2956201
nurglexxx <ben.vees@gmail.com>
2021-07-25 18:31:31 +0200

    Removed Index View, added Error Handling for Overview if Backend is not reachable, added nexus Logo to Navbar & Link

--- NexusWebUI/presenter/views.py
+++ NexusWebUI/presenter/views.py
@@ -9,72 +9,41 @@ from .rpc_requests import get_latest_blocks, get_meta_info, socket_connect, sock
     get_account_works, get_account_payouts
 
 
-# INDEX
-class IndexView(TemplateView):
-    template_name = 'presenter/presenter_index.html'
-    extra_context = {"NonVoiceIndex": 'active'}
-
-
 def block_overview_list(request):
     template_name = 'presenter/overview_list.html'
 
     # Todo Get from .env
-    socket = socket_connect(_ip='127.0.0.1', _port=5000)
+    try:
+        socket = socket_connect(_ip='127.0.0.1', _port=5000)
 
-    # Todo if Socket = None send Message to User
+        # Get the Data for the Main Table
+        latest_block_json = get_latest_blocks(_socket=socket)
+        table_data = OverviewTable(latest_block_json['result'])
 
-    # Get the Data for the Main Table
-    latest_block_json = get_latest_blocks(_socket=socket)
-    table_data = OverviewTable(latest_block_json['result'])
+        # Get the Meta Info
+        meta_info_json = get_meta_info(_socket=socket)
+        pool_hashrate = meta_info_json['result']['pool_hashrate']
+        network_hashrate = meta_info_json['result']['network_hashrate']
+        payout_threshold = meta_info_json['result']['payout_threshold']
+        fee = meta_info_json['result']['fee']
 
-    # Get the Meta Info
-    meta_info_json = get_meta_info(_socket=socket)
-    pool_hashrate = meta_info_json['result']['pool_hashrate']
-    network_hashrate = meta_info_json['result']['network_hashrate']
-    payout_threshold = meta_info_json['result']['payout_threshold']
-    fee = meta_info_json['result']['fee']
+        socket_disconnect(_socket=socket)
 
-    socket_disconnect(_socket=socket)
+        return render(request, template_name, {'table': table_data,
+                                               'pool_hashrate': pool_hashrate,
+                                               'network_hashrate': network_hashrate,
+                                               'payout_threshold': payout_threshold,
+                                               'fee': fee,
+                                               })
 
-    return render(request, template_name, {'table': table_data,
-                                           'pool_hashrate': pool_hashrate,
-                                           'network_hashrate': network_hashrate,
-                                           'payout_threshold': payout_threshold,
-                                           'fee': fee,
-                                           })
+    except Exception as ex:
+        # Todo log Exception
+        messages.error(request, 'Could not establish connection to the Backend Server')
+        return redirect('presenter:error')
 
 
-def block_detail(request, block_id):
-    template_name = 'presenter/block_detail.html'
-    # Todo Get from .env
-    url = "http://127.0.0.1:5000/"
-
-    # Todo get Infos from Method
-    # block = '12345'
-    # hash = '23421342353434gjhgjk34kg51345f345jkf341j5jf435f435hfjhff'
-    # proof_hash = 'aergearger34t3ghtjg34i8t6guz4t34tk787g43kq3tuk347t8uig43'
-    # reward = 2.33
-    # time = '2021-01-01 00:00:00 UTC'
-
-    json_object = block_id
-
-    return render(request, template_name, {'json_object': json_object})
-
-    # return render(request, template_name, {'block': block,
-    #                                        'hash': hash,
-    #                                        'proof_hash': proof_hash,
-    #                                        'reward': reward,
-    #                                        'time': time,
-    #                                        'channel',
-    #                                        'difficulty',
-    #                                        'none',
-    #                                        'bits',
-    #                                        'size',
-    #                                        'version',
-    #                                        'previous',
-    #                                        'next',
-    #                                        'merkle',
-    #                                        })
+class ErrorView(TemplateView):
+    template_name = 'presenter/error.html'
 
 
 def wallet_detail(request):
