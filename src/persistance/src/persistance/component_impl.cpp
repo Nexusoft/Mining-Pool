7dfde8b0b28cf0e5487b73344a6b164566ee4e2a
beuschl <beuschl@hotmail.com>
2021-07-31 15:32:40 +0200

    fixed persistance component intialization

--- src/persistance/src/persistance/component_impl.cpp
+++ src/persistance/src/persistance/component_impl.cpp
@@ -1,4 +1,5 @@
 #include "persistance/component_impl.hpp"
+#include "persistance/data_access_factory_impl.hpp"
 #include <spdlog/spdlog.h>
 
 namespace nexuspool {
@@ -7,6 +8,7 @@ namespace persistance {
 Component_impl::Component_impl(std::shared_ptr<spdlog::logger> logger, config::Persistance_config config)
     : m_logger{std::move(logger)}
     , m_config{std::move(config)}
+    , m_data_access_factory{std::make_shared<Data_access_factory_impl>(m_logger, m_config)}
 {
 }
 
