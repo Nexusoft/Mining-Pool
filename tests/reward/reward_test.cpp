3c04b33a39bc0e46deecd5bdabb34038733cf4ad
beuschl <beuschl@hotmail.com>
2021-08-04 10:08:04 +0200

    reward component unit test

--- tests/reward/reward_test.cpp
+++ tests/reward/reward_test.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
-#include "reward/manager.hpp"
+#include "reward/create_component.hpp"
+
+using namespace ::nexuspool;
 
 TEST(Reward, initialisation)
 {
-	nexuspool::reward::Manager::Uptr reward_manager{};
-	EXPECT_FALSE(reward_manager);
+	auto reward_component = reward::create_component();
+	EXPECT_TRUE(reward_component);
+
+	auto reward_manager = reward_component->create_reward_manager();
+	EXPECT_TRUE(reward_manager);
+}
+
+TEST(Reward, difficulty_test)
+{
+	auto reward_component = reward::create_component();
+	auto reward_manager = reward_component->create_reward_manager();
+
+	auto result = reward_manager->check_difficulty(0, 0);
 }
