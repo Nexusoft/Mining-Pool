b906e54cc2d7e3a3f7f86c16c1bf01175ad2d997
beuschl <beuschl@hotmail.com>
2021-08-29 10:11:02 +0200

    end round logic ind reward component

--- src/reward/src/reward/component_impl.cpp
+++ src/reward/src/reward/component_impl.cpp
@@ -13,11 +13,36 @@ Component_impl::Component_impl(std::shared_ptr<spdlog::logger> logger, persistan
 
 void Component_impl::start_round()
 {
-    m_shared_data_writer->create_round();
+    if (!m_shared_data_writer->create_round())
+    {
+        m_logger->error("Failed to create a new round!");
+    }
 }
 
 bool Component_impl::end_round(std::uint32_t round_number)
 {
+    auto const round_data = m_data_reader->get_latest_round();
+    if (round_data.m_round != round_number)
+    {
+        m_logger->error("Failed to end round {}. Latest round is {}", round_number, round_data.m_round);
+        return false;
+    }
+
+    if (!round_data.m_is_active)
+    {
+        m_logger->error("Failed to end round {}. Round is already ended.", round_number);
+        return false;
+    }
+
+    if (!round_data.m_is_paid)
+    {
+        // payout round
+    }
+
+    // end round now
+    round_data.m
+
+
     return true;
 }
 
