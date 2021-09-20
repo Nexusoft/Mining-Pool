f8c611c8d533115d61a708f49df925f80f944495
beuschl <beuschl@hotmail.com>
2021-09-20 14:05:16 +0200

    initialize persistance data types

--- src/persistance/inc/persistance/types.hpp
+++ src/persistance/inc/persistance/types.hpp
@@ -10,53 +10,63 @@ namespace persistance {
 
 struct Account_data
 {
-	std::string m_address;
-	std::uint16_t m_connections;
-	std::string m_created_at;
-	std::string m_last_active;
-	double m_shares;
-	double m_hashrate;
+	std::string m_address{};
+	std::uint16_t m_connections{};
+	std::string m_created_at{};
+	std::string m_last_active{};
+	double m_shares{ 0 };
+	double m_hashrate{0};
+
+	bool is_empty() const { return (m_address.empty()); }
 };
 
 struct Account_data_for_payment
 {
-	std::string m_address;
-	double m_shares;
+	std::string m_address{};
+	double m_shares{ 0 };
+
+	bool is_empty() const { return (m_address.empty()); }
 };
 
 struct Block_data
 {
-	std::string m_hash;
-	std::uint32_t m_height;
-	std::string m_type;
-	double m_difficulty;
-	bool m_orphan;
-	std::string m_block_finder;
-	std::uint32_t m_round;
-	std::string m_block_found_time;
-	double m_mainnet_reward;
+	std::string m_hash{};
+	std::uint32_t m_height{ 0 };
+	std::string m_type{};
+	double m_difficulty{ 0 };
+	bool m_orphan{ false };
+	std::string m_block_finder{};
+	std::uint32_t m_round{ 0 };
+	std::string m_block_found_time{};
+	double m_mainnet_reward{ 0 };
+
+	bool is_empty() const { return (m_hash.empty()); }
 };
 
 struct Round_data
 {
-	std::int64_t m_round;
-	double m_total_shares;
-	double m_total_rewards;
-	std::uint32_t m_blocks;
-	std::uint32_t m_connection_count;
-	std::string m_start_date_time;
-	std::string m_end_date_time;
-	bool m_is_active;
-	bool m_is_paid;
+	std::int64_t m_round{ 0 };
+	double m_total_shares{ 0 };
+	double m_total_rewards{ 0 };
+	std::uint32_t m_blocks{ 0 };
+	std::uint32_t m_connection_count{ 0 };
+	std::string m_start_date_time{};
+	std::string m_end_date_time{};
+	bool m_is_active{ false };
+	bool m_is_paid{ false };
+
+	bool is_empty() const { return (m_round == 0); }
 };
 
 struct Payment_data
 {
-	std::string m_account;
-	double m_amount;
-	double m_shares;
-	std::string m_payment_date_time;
-	std::int64_t m_round;
+	std::string m_account{};
+	double m_amount{ 0 };
+	double m_shares{ 0 };
+	std::string m_payment_date_time{};
+	std::int64_t m_round{ 0 };
+
+	bool is_empty() const { return (m_account.empty()); }
 };
 
 struct Config_data
